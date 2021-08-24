e2eb64b48fe98e8b7255213de6ed0cff7306004e
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-08-24 02:39:44 -0700

    Removed CSS, did not want to bother with Sass. Added jumbotron in place of navbar

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -89,16 +89,18 @@ const generateTeamPage = function (employeeCards) {
         <title>Dental Team Profile</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
         <link href="https://fonts.googleapis.com/css2?family=Jost:ital@1&family=Roboto&display=swap" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link rel="stylesheet" href="./stylesheet.css">
         <script src="https://kit.fontawesome.com/d36c7c889c.js" crossorigin="anonymous"></script>
     </head>
     <body>
         <header>
-        <nav class="navbar navbar-light" style="background-color: #1e000e;">
-        <div class="container-fluid">
-          <span class="navbar-brand mb-0 h1">Dental Team Profile</span>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Dental Team Profile</h1>
+                <p class="lead">Names, Roles, Employee IDs, Emails</p>
+            </div>
         </div>
-      </nav>
         </header>
         <main>
             <div class="container">
